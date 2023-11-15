c63ad0fabbb21e871b4e27b65aabc3197bb234a9
Jorel Masgonty <jorel.masgonty@gmail.com>
2023-11-15 10:24:07 +0100

    changement

--- sequence4/tableaux-indexes/saisie-notes.php
+++ sequence4/tableaux-indexes/saisie-notes.php
@@ -1,1 +1,14 @@
 <?php
+
+// crire un programme saisie-notes.php demandant à l'utilisateur
+//de saisir des notes (comprises entre 0 et 20) puis d'afficher le
+//nombre de notes saisies ainsi que les notes saisies.
+
+$note = readline("veuillez saisir une note entre 0 et 20 (q pour arrêter) :");
+
+while ($note!="q"){
+    $note = readline("veuillez saisir une note entre 0 et 20 (q pour arrêter) :");
+    if($note <0 || $note>20){
+        echo "La note saisie est incorrecte";
+    }
+}

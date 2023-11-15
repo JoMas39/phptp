31124bf87790e948ed5fdfe1d066a4301a93da15
Jorel Masgonty <jorel.masgonty@gmail.com>
2023-11-15 11:51:14 +0100

    gpo

--- sequence4/tableaux-indexes/test.php
+++ sequence4/tableaux-indexes/test.php
@@ -1,24 +1,16 @@
 <?php
-// crire un programme saisie-notes.php demandant à l'utilisateur
-//de saisir des notes (comprises entre 0 et 20) puis d'afficher le
-//nombre de notes saisies ainsi que les notes saisies.
+$array=[];
+$note=readline("veuillez saisir une note entre 0 et 20 (q pour arrêter) : ");
 
-$notes = [];
-for ($i=0; $i !="q" ; $i++) {
-
-    $notes[$i] = readline("veuillez saisir une note entre 0 et 20 (q pour arrêter) : ");
-
-    if ($notes[$i]=="q"){
-        echo "Arrêt du programme", "\n";
-        break;
-    }
-
-    elseif($notes[$i] <0 || $notes[$i]>20){
+while ($note!="q"){
+    if ($note <0 || $note>20){
         echo "La note saisie est incorrecte", "\n";
+        $note=readline("veuillez saisir une note entre 0 et 20 (q pour arrêter) : ");
     }
+    $note=readline("veuillez saisir une note entre 0 et 20 (q pour arrêter) : ");
 }
-echo "Vous avez saisi " .count($notes). " notes", "\n";
-echo "Les notes saisies sont : ";
-foreach ($notes as $note) {
-    echo $note, " ";
-}
+
+
+
+
+//array_push()

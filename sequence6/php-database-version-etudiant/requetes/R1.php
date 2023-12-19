4ab0f012fe7109d27fc0fd2dd3c0b5a21b71a961
Jorel Masgonty <jorel.masgonty@gmail.com>
2023-12-19 08:45:12 +0100

    tp

--- sequence6/php-database-version-etudiant/requetes/R1.php
+++ sequence6/php-database-version-etudiant/requetes/R1.php
@@ -5,20 +5,7 @@ require_once "../base-de-donnees/tableAuteur.php";
 require_once "../base-de-donnees/tableCategorie.php";
 require_once "requetes.php";
 
-/*
- * Test requête R1
-*/
-
-
 echo "...................................................................................... \n";
-
-// Appel de la fonction
-$resultatsArticlesActifs = rechercherArticlesActifs($tableArticles);
-
-// Affichage des résultats
-foreach ($resultatsArticlesActifs as $resultat) {
-    echo "ID: {$resultat['id']}, Titre: {$resultat['titre']}, Contenu: {$resultat['contenu']}, Date de création: {$resultat['date_creation']}\n";
-    echo PHP_EOL;
-}
-
+$resultats = rechercherArticlesActifs($tableArticles);
+print_r($resultats);
 echo "......................................................................................";

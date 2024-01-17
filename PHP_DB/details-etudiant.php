6fa36f350c06d5d745af5b1229bc0f3f8cb16530
Jorel Masgonty <jorel.masgonty@gmail.com>
2024-01-17 10:28:41 +0100

    suite

--- PHP_DB/details-etudiant.php
+++ PHP_DB/details-etudiant.php
@@ -7,11 +7,18 @@ require "config/db-config.php";
 // Préparer la requête
 $requete=$connexion->prepare("SELECT * FROM etudiant WHERE id_etudiant= :id");
 // Donner une valeur au paramète
-$requete->bindValue(":id",2);
+$requete->bindValue(":id",50);
 //Exécuter la requête
 $requete->execute();
 //Récupérer les données
 $etudiant=$requete->fetch(PDO::FETCH_ASSOC);
+// Test si l'étudiant existe
+if ($etudiant) {
 //$etudiant est un tableau associatif correspondant à l'enregistrement recherché
 // Affichage des résultats
-print_r($etudiant);
+    print_r($etudiant);
+} else {
+    echo "L'étudiant recherché n'existe pas !" ;
+}
+
+// Modifier le programme pour que l'user saisisse un id
---
+++ PHP_DB/readline-etudiant.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// Modifier le programme pour que l'user saisisse un id
+
+
+/**
+ * @var PDO $connexion
+ */
+require "config/db-config.php";
+
+// Saisie par l'user
+$choix=readline("Veuillez saisir l'id de l'étudiant : ");
+
+// Préparer la requête
+$requete = $connexion->prepare("SELECT * FROM etudiant WHERE id_etudiant= :id");
+// Donner une valeur au paramète
+$requete->bindValue(":id", $choix);
+//Exécuter la requête
+$requete->execute();
+//Récupérer les données
+$etudiant = $requete->fetch(PDO::FETCH_ASSOC);
+// Test si l'étudiant existe
+if ($etudiant) {
+//$etudiant est un tableau associatif correspondant à l'enregistrement recherché
+// Affichage des résultats
+    print_r($etudiant);
+} else {
+    echo "L'étudiant recherché n'existe pas !";
+}
